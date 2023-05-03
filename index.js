9e4da9065da6b74cb47141c4f4ebde06121016a0
Jedica <Clementjedica@gmail.com>
2023-05-03 16:52:42 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,98 @@
+//declaration des variables
+
+//var
+ var nombre = [1, 2, 3, 5];
+ console.log(nombre[2]);
+/********/
+ var eleve = {
+    nom: "elyse",
+    prenom: "jedica"
+ };
+ console.log(eleve.nom);
+
+//let
+ let chiffre = [1, 9, 7, 3, 6];
+ console.log(chiffre[4]);
+/*********/
+let indentiter = {
+    nom: "sanda",
+    prenom: "anjahina"
+};
+console.log(indentiter.prenom);
+
+//const
+const fraisAller = 100;
+const fraisDeRetour = 150;
+let totalFrais = fraisAller + fraisDeRetour;
+console.log(totalFrais);
+/********/
+const presence = {
+    name:"toky" + "fy"
+};
+console.log(presence.name); 
+
+//*****suivant*****/
+
+//var
+var nombre = [1, 2, 3, 5];
+console.log(nombre[2]);
+/********/
+var eleve = {
+   nom: "ABC",
+   prenom: "HKKJG"
+};
+console.log(eleve.nom);
+
+//let
+let tableau = [1, 9, 7, 3, 6];
+console.log(tableau[4]);
+/*********/
+let indent = {
+   nom: "PAPA",
+   prenom: "KKKKK"
+};
+console.log(indent.prenom);
+
+//const
+const cout = 200;
+const money = 200;
+let retour =  money - cout;
+console.log(retour);
+/********/
+const example = {
+   name:"nafindra" + " " + "toerana"
+};
+console.log(example.name);
+
+//*****suivant*****/
+
+//var
+var nombre = [1, 2, 3, 5];
+console.log(nombre[2]);
+/********/
+var eleve = {
+   nom: "ABC",
+   prenom: "HKKJG"
+};
+console.log(eleve.nom);
+
+//let
+let table = [1, 9, 7, 3, 6];
+console.log(table[4]);
+/*********/
+let trying = {
+   nom: "PAPA",
+   prenom: "KKKKK"
+};
+console.log(trying.prenom);
+
+//const
+const Prix_Achat = 100;
+const argent = 150;
+let totalprix =  argent - Prix_Achat;
+console.log(totalprix);
+/********/
+const appel = {
+   name:"toky" + "fy"
+};
+console.log(appel.name);
